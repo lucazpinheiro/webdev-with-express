25f841d9674d7fa40f5c78f64548b3b02c533d05
Lucas <lucascpinheiro10@gmail.com>
2021-10-03 15:35:07 -0300

    chapter 4

--- meadwlark/meadowlark.js
+++ meadwlark/meadowlark.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const getFortune = require('./lib/fortune')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -14,17 +15,10 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
 
-const fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-]
 // routes
 app.get('/', (req, res) => res.render('home'))
 app.get('/about', (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+  const randomFortune = getFortune()
   res.render('about', { fortune: randomFortune })
 })
 
