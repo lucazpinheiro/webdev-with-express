419652b9e61477f74813e4fcf0c45fa3d3ed2bc2
Lucas <lucascpinheiro10@gmail.com>
2021-10-06 20:00:28 -0300

    refact: add express app exportation for testing

--- meadwlark/meadowlark.js
+++ meadwlark/meadowlark.js
@@ -25,6 +25,11 @@ app.use(handlers.notFound)
 // custom 500 page
 app.use(handlers.serverError)
 
-app.listen(port, () => console.log(
-  `Express started on http://localhost:${port}; ` +
-  `press Ctrl-C to terminate.`))
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Express started on http://localhost:${port}; ` + `press Ctrl-C to terminate.`)
+  })
+} else {
+  module.exports = app
+}
+
